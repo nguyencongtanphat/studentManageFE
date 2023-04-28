b33f53488fb4e4dab63626abf59af68cd09223b1
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-04-28 22:38:33 +0700

    solve conflic

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import ErrorPage from "./modules/errorPage/ErrorPage";
 import Root from "./Root";
 import HomePage from "./modules/homePage/HomePage";
-<<<<<<< Updated upstream
 import Allstudent from "./modules/students/screen/allstudent/Allstudent";
 import Addstudent from "./modules/students/screen/addstudent/Addstudent";
 import Editstudent from "./modules/students/screen/editstudent/Editstudent";
@@ -13,12 +12,8 @@ import Profilestudent from "./modules/students/screen/profilestudent/Profilestud
 import Allgrade from "./modules/grade/Allgrade/Allgrade";
 import Gradesinformation from "./modules/grade/Gradesinformation/Gradesinformation";
 
-=======
 import AllStudent from "./modules/students/screens/AllStudent";
-import ClassInformation from "./modules/classes/classInfoPage";
-import ClassesPage from "./modules/classes/classesPage";
-import classesAdd from "./modules/classes/classesAdd";
->>>>>>> Stashed changes
+
 
 const router = createBrowserRouter([
   {
@@ -38,7 +33,6 @@ const router = createBrowserRouter([
         errorElement:<ErrorPage/>
       },
       {
-<<<<<<< Updated upstream
         path:"/addstudents",
         element:<Addstudent/>,
         errorElement:<ErrorPage/>
@@ -63,26 +57,6 @@ const router = createBrowserRouter([
         element:<Gradesinformation/>,
         errorElement:<ErrorPage/>
       },
-=======
-        path:"classes",
-        element: <ClassesPage/>,
-        errorElement: <errorpage/>,
-        children: [
-          {
-            path:"id",
-            element: <ClassInformation/>,
-            errorElement: <errorpage/>,
-            Children: [
-              {
-                path:"addclass",
-                element: <classesAdd/>,
-                errorElement: <errorpage/>
-              },
-            ]
-          },
-        ]
-      }
->>>>>>> Stashed changes
     ]
   }
 ])
