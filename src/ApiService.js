9f534d94e0bbb9a9392e4d5a1837e1e9efa23362
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-05-05 16:29:22 +0700

    conflict with be

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 
-const urlServer = "https://student-management-lu0c.onrender.com"
+//const urlServer = "https://student-management-lu0c.onrender.com"
+const urlServer = "http://localhost:3000";
 
 class ApiService{
     static get = async (reqUrl)=>{
         try{
-          return await axios.get(urlServer + "/" + reqUrl);
+          const response =  await axios.get(urlServer + "/" + reqUrl);
+          return response.data.ResponseResult.result;
         }catch(e){
             throw e;
         }
--- src/modules/students/screen/allstudent/Allstudent.jsx
+++ src/modules/students/screen/allstudent/Allstudent.jsx
@@ -1,28 +1,93 @@
-import React from "react";
-import {
-  Table,
-  Select,
-  Card,
-  Typography,
-  Space,
-  Button,
-  AutoComplete,
-} from "antd";
+import React, { useState } from "react";
+import { Table, Select, Card, Space, Button, AutoComplete } from "antd";
 import style from "./Allstudent.module.css";
+import { useEffect } from "react";
+import ApiService from "../../../../ApiService";
 
 function Allstudent() {
+  const [studentListView, setStudentListView] = useState([]);
+  const [classList, setClassList] = useState([]);
+  const [nameQuery, setNameQuery] = useState("");
+  const [classQuery, setClassQuery] = useState("");
+  const [studentList, setStudentList] = useState([]);
+
+  //fetch data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const resultStudent = await ApiService.get("students");
+        console.log(resultStudent);
+        const tempStudentList = resultStudent.map((student) => {
+          return {
+            key: student.idStudent,
+            id: student.idStudent,
+            name: student.fullName,
+            class: student.name,
+          };
+        });
+        setStudentListView(tempStudentList);
+        setStudentList(tempStudentList);
+      } catch (e) {
+        console.log("error:", e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  const columns = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Class",
+      dataIndex: "class",
+      key: "class",
+    },
+  ];
+
+  const searchHandler = () => {
+    let newStudentList = studentList;
+    if (nameQuery && classQuery) {
+      newStudentList = studentList.filter((student) => {
+        return student.name.includes(nameQuery) && student.class === classQuery;
+      });
+    } else if (nameQuery) {
+      newStudentList = studentList.filter((student) => {
+        return student.name.includes(nameQuery);
+      });
+    } else if (classQuery) {
+      newStudentList = studentList.filter((student) => {
+        return student.class === classQuery;
+      });
+    }
+
+    console.log("new students: ", newStudentList);
+    setStudentListView(newStudentList);
+  };
+
   return (
     <div className={style.Allstudent}>
-      <Card title= "All Student Data">
+      <Card title="All Student Data">
         <div className={style.selectClass}>
           <Space>
             <AutoComplete
               style={{ width: 200 }}
-              onSearch={() => {}}
+              onSearch={(value) => {
+                setNameQuery(value);
+              }}
               placeholder="Search by name"
             />
             <Select
-              onChange={(value) => {}}
+              onChange={(value) => {
+                setClassQuery(value);
+              }}
               defaultValue={"Select class"}
               options={[
                 {
@@ -63,27 +128,20 @@ function Allstudent() {
                 },
               ]}
             ></Select>
-            <Button htmlType="search" type="primary">
+            <Button onClick={searchHandler} htmlType="search" type="primary">
               Search
             </Button>
           </Space>
         </div>
         <Table
-          columns={[
-            {
-              title: "ID",
+          columns={columns}
+          dataSource={studentListView}
+          onRow={(record) => ({
+            onClick: () => {
+              console.log(record);
             },
-            {
-              title: "Name",
-            },
-            {
-              title: "Class",
-            },
-          ]}
-          pagination={{
-            pageSize: 7,
-          }}
-        ></Table>
+          })}
+        />
       </Card>
     </div>
   );
