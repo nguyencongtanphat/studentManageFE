1e414d5e90b2e1956a40ce208a7ee8d9a9bb4aa1
Phát Nguyễn Công Tấn <20521734@ms.uit.edu.vn>
2023-05-12 23:02:09 +0700

    complete add new student

--- src/modules/students/screen/addstudent/Addstudent.jsx
+++ src/modules/students/screen/addstudent/Addstudent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import moment from "moment";
 import {
   Card,
@@ -15,21 +15,43 @@ import ApiService from "../../../../ApiService";
 
 function Addstudent() {
   const [form] = Form.useForm();
+  const [classCurrentSemester, setClassCurrentSemester] = useState([]);
+  const [currentSemesterId, setCurrentSemester] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const currentSemesterId = await ApiService.get(
+        "parameters?name=CurrentSemesterId"
+      );
+      setCurrentSemester(currentSemesterId.value);
+      const classes = await ApiService.get(
+        `classes?semesterId=${currentSemesterId.value}`
+      );
+      console.log("currentClass: ", classes);
+      setClassCurrentSemester(classes);
+    };
+    getData();
+  }, []);
   const submitHandler = async () => {
     const formValues = form.getFieldsValue();
     const dateOfBirth = formValues.dateOfBirth["$d"];
     formValues.dateOfBirth = dateOfBirth
+    formValues.currentSemesterId = currentSemesterId;
     console.log("Form values:", formValues);
     await ApiService.post("students", formValues)
   };
   
   function disabledDate(current) {
-    // Tính toán ngày giới hạn
-    const minDate = moment().subtract(18, "years").startOf("day");
-    const maxDate = moment().add(16, "days").startOf("day");
-    // Kiểm tra xem ngày hiện tại có nằm trong khoảng giới hạn không
-    return !current.isBetween(minDate, maxDate, null, "[]");
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+   const startYear = moment(`${currentYear-18}-01-01`, 'YYYY-MM-DD').startOf('year');
+  // Lấy năm cuối cùng trong khoảng giới hạn
+  const endYear = moment(`${currentYear - 16}-12-31`, "YYYY-MM-DD").endOf(
+    "year"
+  );
+  // Nếu năm được chọn nằm trong khoảng giới hạn thì cho phép chọn năm đó, ngược lại không cho phép
+  return !(current >= startYear && current <= endYear);
   }
+  
   return (
     <div className={style.Addstudent}>
       <Card title="Add Students">
@@ -42,25 +64,15 @@ function Addstudent() {
             <Input size="medium" placeholder=""></Input>
           </Form.Item>
           <Form.Item
-            name={"class"}
+            name={"classId"}
             label="CLASS"
             rules={[{ required: true, message: "Please choose your class!" }]}
           >
             <Select placeholder="Please select class">
-              {[
-                "10A1",
-                "10A2",
-                "10A3",
-                "11A1",
-                "11A2",
-                "11A3",
-                "12A1",
-                "12A2",
-                "12A3",
-              ].map((className) => {
+              {classCurrentSemester.map((item) => {
                 return (
-                  <Select.Option value={className} key={className}>
-                    {className}
+                  <Select.Option value={item.idClass} key={item.idClass}>
+                    {item.name}
                   </Select.Option>
                 );
               })}
@@ -73,7 +85,7 @@ function Addstudent() {
               { required: true, message: "Please choose your birth date!" },
             ]}
           >
-            <DatePicker  />
+            <DatePicker disabledDate={disabledDate} />
           </Form.Item>
           <Form.Item
             name={"gender"}
