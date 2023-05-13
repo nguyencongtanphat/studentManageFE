e47cb8677c2aea4b5be20bb335ca10583a3ebb74
khoagman <khoagman@gmail.com>
2023-05-13 08:48:51 +0700

    edit profile of student

--- src/modules/students/screen/profilestudent/Profilestudent.jsx
+++ src/modules/students/screen/profilestudent/Profilestudent.jsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import Alert from 'antd';
+import dayjs from 'dayjs';
+import ApiService from '../../../../ApiService';
+
 import { 
     Checkbox,
     DatePicker,
@@ -12,50 +17,124 @@ import {
 } from 'antd'
 
 import style from './Profilestudent.module.css'
+import CompoundedSpace from 'antd/es/space';
 const { Title } = Typography;
 
 function Profilestudent () {
     const [componentDisabled, setComponentDisabled] = useState(true);
+    const [editButton, setEditButton] = useState(true);
+    const [student, setStudent] = useState({}); 
+    const [form] = Form.useForm()
+
+    const {id} = useParams();
+    const requrl = "students/"+id;
+
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            console.log(requrl);
+            const resultStudent = await ApiService.get(requrl);
+            const tempStudent = {
+                key: resultStudent.idStudent,
+                name: resultStudent.fullName,
+                class: resultStudent.name,
+                address: resultStudent.address,
+                birth: resultStudent.dayOfBirth,
+                gender: resultStudent.gender,
+                email: resultStudent.Email
+            };
+            setStudent(tempStudent);
+
+        } catch (e) {
+            console.log("error:", e);
+        }
+        };
+        fetchData();
+    }, [id]);
+
+    const FieldEnable = () => {
+        setComponentDisabled(!componentDisabled);
+        setEditButton(!editButton);
+    }
+
+    const HandleSubmit = () => {
+        const {name,address,birth,gender,email} = form.getFieldsValue();
+        if (!name && !address && !birth && !gender && !email)
+            console.log('dien it nhat 1 thong tin di pa');
+        else {
+            const newStudent = {
+                fullName: name ? name: student.name,
+                address: address ? address: student.address,
+                dayOfBirth: birth ? birth: student.birth,
+                gender: gender ? gender: student.gender,
+                Email: email ? email: student.email
+            }
+            const putStudent = async (data) => {
+                const putdata = await ApiService.put(requrl, data)        
+                console.log(putdata);
+            }
+            putStudent(newStudent);
+            setComponentDisabled(!componentDisabled);
+            setEditButton(!editButton);
+        }
+    }
+
     return (
         <div className={style.Addstudent}>
             <Card title= "Profile Student">
-                        <Form>
+                        <Form
+                            form={form}
+                        >
                             <Form.Item name={"name"} label="NAME*">
                                 <Input
-                                    size="medium"
-                                    placeholder=""
-                                    initialValue='aaa'></Input>
+                                    placeholder={student.name}
+                                    disabled={componentDisabled}
+                                />
                             </Form.Item>
-                            <Form.Item name={"class"} label="CLASS*">
-                                <Select placeholder="Please select class">
-                                    {["10A1","10A2","10A3","11A1","11A2","11A3","12A1","12A2","12A3"].map(gender=>{
-                                        return <Select.Option value={gender} key={gender}>{gender}</Select.Option>
-                                    })}
-                                </Select>
-                            </Form.Item>
-                            <Form.Item name={"dateofbirth"} label="DATE OF BIRTH*">
-                                <DatePicker />
-                            </Form.Item>
-                            <Form.Item name={"id"} label="ID*">
-                                <Input size="medium" placeholder=""></Input>
+                            <Form.Item name={"birth"} label="DATE OF BIRTH*">
+                                <DatePicker
+                                    disabled={componentDisabled}
+                                    placeholder={student.birth}
+                                />
                             </Form.Item>
                             <Form.Item name={"gender"} label="GENDER*">
-                                <Select placeholder="Please select gender">
+                                <Select placeholder={student.gender} disabled={componentDisabled}>
                                     {["Nam","Nữ"].map(gender=>{
                                         return <Select.Option value={gender} key={gender}>{gender}</Select.Option>
                                     })}
                                 </Select>
                             </Form.Item>
                             <Form.Item name={"address"} label="ADDRESS*">
-                                <Input size="medium" placeholder=""></Input>
+                                <Input
+                                    size="medium"
+                                    placeholder={student.address}
+                                    disabled={componentDisabled}
+                                />
                             </Form.Item>
                             <Form.Item name={"email"} label="EMAIL*">
-                                <Input size="medium" placeholder=""></Input>
+                                <Input
+                                    size="medium"
+                                    placeholder={student.email}
+                                    disabled={componentDisabled}
+                                />
                             </Form.Item>
                                 <div className={style.spaceButton}>
                                     <Space wrap>
-                                        <Button htmlType='submit' type='primary'>Edit</Button>
-                                        {/* <Button htmlType='submit' type='primary'>Save</Button> */}
+                                        {editButton && <Button
+                                            htmlType='submit'
+                                            type='primary'
+                                            onClick={() => {FieldEnable()}}
+                                            >
+                                                Edit
+                                            </Button>}
+                                        {!editButton && <Button
+                                            htmlType='submit'
+                                            type='primary'
+                                            onClick={() => {HandleSubmit()}}
+                                            danger
+                                            >
+                                                Save
+                                            </Button> }
                                     </Space> 
                                 </div> 
                         </Form>
